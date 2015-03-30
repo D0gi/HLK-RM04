93fd4b4440da084a9db48d57d63ad6f8e53e8e16
D0gi <s122106@hft-leipzig.de>
2015-03-30 12:20:35 +0200

    add script to load .bin to tftp

--- cpdoghlk.sh
+++ cpdoghlk.sh
@@ -1,3 +1,12 @@
-sudo cp -v bin/ramips/openwrt-ramips-rt305x-hlk-rm04-squashfs-sysupgrade.bin /var/lib/tftpboot/doghlk.bin
-ls -l /var/lib/tftpboot/doghlk.bin
+#!/bin/sh
+ROT=`tput setaf 1`
+GREEN=`tput setaf 2`
 
+mem=$(cat target/linux/ramips/rt305x/config-3.10 | egrep -o "mem=[16|32]{2}" | egrep -o "[16|32]{2}")
+sudo cp -v bin/ramips/openwrt-ramips-rt305x-hlk-rm04-squashfs-sysupgrade.bin /var/lib/tftpboot/"$mem"doghlk.bin
+ls -l /var/lib/tftpboot/"$mem"doghlk.bin
+echo "${GREEN} $mem"
+
+
+
+ 
